3d6a83edda0ba489a643be3c2e1e584240511f84
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-14 12:25:43 +0200

    Jetty-plugin used to set up webapp on server

---
+++ mywebapp/src/main/java/com/topyfi/javabrains/MyClass.java
@@ -0,0 +1,9 @@
+package com.topyfi.javabrains;
+
+import java.util.*;
+
+public class MyClass{
+    public static void main(String[] args){
+        List<String> stringList = new ArrayList<String>();
+    }
+}
